ef1eeb5afc638a7a78ad4a15499f4751a8118dc6
Raj Jayswal <rajjayswal480@gmail.com>
2023-10-17 19:32:42 +0530

    first commit

---
+++ src/Pages/TagPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import Pagination from '../components/Pagination';
+import Blogs from '../components/Blogs';
+
+
+const TagPage = () => {
+
+    const navigation = useNavigate();
+    const location = useLocation();
+    const tag = location.pathname.split("/").at(-1);
+  return (
+    <div>
+        <Header/>
+        <div>
+
+          
+            <h2>
+                Blogs Tagged <span>#{tag}</span>
+            </h2>
+        </div>
+        <Blogs/>
+        <button 
+            onClick={() => navigation(-1)}
+            >
+                Back
+            </button>
+        <Pagination/>
+      
+    </div>
+  )
+}
+
+export default TagPage
